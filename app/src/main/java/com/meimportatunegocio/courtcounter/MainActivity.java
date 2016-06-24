3e47f2e45f4179b55b3992cc6813bbefa3f472c5
Dennis Zelada <denniszelada@gmail.com>
2016-06-24 18:57:43 -0400

    Add the two teams score buttons

--- app/src/main/java/com/meimportatunegocio/courtcounter/MainActivity.java
+++ app/src/main/java/com/meimportatunegocio/courtcounter/MainActivity.java
@@ -2,12 +2,60 @@ package com.meimportatunegocio.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addOnePointTeamA(View v) {
+        scoreTeamA += 1;
+        displayScoreTeamA(scoreTeamA);
+    }
+
+
+    public void addTwoPointsTeamA(View v) {
+        scoreTeamA += 2;
+        displayScoreTeamA(scoreTeamA);
+    }
+
+
+    public void addThreePointsTeamA(View v) {
+        scoreTeamA += 3;
+        displayScoreTeamA(scoreTeamA);
+    }
+
+    public void addOnePointTeamB(View v) {
+        scoreTeamB += 1;
+        displayScoreTeamB(scoreTeamB);
+    }
+
+
+    public void addTwoPointsTeamB(View v) {
+        scoreTeamB += 2;
+        displayScoreTeamB(scoreTeamB);
+    }
+
+
+    public void addThreePointsTeamB(View v) {
+        scoreTeamB += 3;
+        displayScoreTeamB(scoreTeamB);
+    }
+
+    public void displayScoreTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayScoreTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
     }
 }
